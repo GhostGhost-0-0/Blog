eaf939bbec3f15eab95e3899c107cb7640eba8aa
PermabanSun <295375895@qq.com>
2022-09-06 14:20:31 +0800

    用于参数校验时的分组

---
+++ sangeng-framework/src/main/java/com/zzx/domain/group/Save.java
@@ -0,0 +1,12 @@
+package com.zzx.domain.group;
+
+/**
+ * @BelongProject: SGBlog
+ * @BelongPackage: com.zzx.domain.group
+ * @Author: 那个小楠瓜
+ * @CreateTime: 2022-08-31 20:27
+ * @Description: 插入校验的标识
+ * @Version: 1.0
+ */
+public interface Save {
+}
---
+++ sangeng-framework/src/main/java/com/zzx/domain/group/Update.java
@@ -0,0 +1,12 @@
+package com.zzx.domain.group;
+
+/**
+ * @BelongProject: SGBlog
+ * @BelongPackage: com.zzx.domain.group
+ * @Author: 那个小楠瓜
+ * @CreateTime: 2022-08-31 20:12
+ * @Description: 更新时的校验标识
+ * @Version: 1.0
+ */
+public interface Update {
+}
