d096985d83e71f697823bb5336562c7df83669db
PermabanSun <295375895@qq.com>
2022-09-06 14:23:28 +0800

    创建用于数据传输的后台博文dto

---
+++ sangeng-framework/src/main/java/com/zzx/domain/vo/ArticleAdminListVo.java
@@ -0,0 +1,26 @@
+package com.zzx.domain.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @BelongProject: SGBlog
+ * @BelongPackage: com.zzx.domain.vo
+ * @Author: 那个小楠瓜
+ * @CreateTime: 2022-09-01 11:47
+ * @Description: 博文后台vo
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ArticleAdminListVo {
+
+    private Long id;
+    private String title;
+    private String summary;
+    private Date createTime;
+}
