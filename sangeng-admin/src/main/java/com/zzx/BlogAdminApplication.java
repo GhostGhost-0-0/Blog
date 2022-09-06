d8947d57a9fce878214505101c23a6896876c40b
PermabanSun <295375895@qq.com>
2022-09-06 14:19:45 +0800

    开启事务

--- sangeng-admin/src/main/java/com/zzx/BlogAdminApplication.java
+++ sangeng-admin/src/main/java/com/zzx/BlogAdminApplication.java
@@ -3,6 +3,7 @@ package com.zzx;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -16,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @MapperScan("com.zzx.domain.mapper")
 @EnableSwagger2
+@EnableTransactionManagement
 public class BlogAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogAdminApplication.class, args);
