e222fc6312d6f9732a06bc0a95ead5089a4de153
PermabanSun <295375895@qq.com>
2022-09-13 14:10:14 +0800

    为用户vo添加了角色id字段

--- sangeng-framework/src/main/java/com/zzx/domain/vo/admin/AdminUserVo.java
+++ sangeng-framework/src/main/java/com/zzx/domain/vo/admin/AdminUserVo.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * @BelongProject: SGBlog
@@ -30,4 +31,6 @@ public class AdminUserVo {
     private String phonenumber;
 
     private Date createTime;
+
+    private List<Long> roleIds;
 }
